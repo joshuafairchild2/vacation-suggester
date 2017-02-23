4c2a658d248755335ef6134d5c58a1d23fa4bf3c
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-02-23 15:11:40 -0800

    add if statements to prompt the user to fill in all fields and some css styling

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,6 @@
 $(document).ready(function() {
   $("select").children("option:first-child").hide();
 
-
-
   $("#q1").submit(function(event) {
     $("#q1").hide();
     $("#q2").show();
@@ -13,9 +11,13 @@ $(document).ready(function() {
   });
 
   $("#q2").submit(function(event) {
-    $("#q2").hide();
-    $("#q3").show();
-
+    var input2 = parseInt($("#season").val());
+    if (input2 === 1) {
+      $("#q2-alert").text("Please make a selection");
+    } else {
+      $("#q2").hide();
+      $("#q3").show();
+    }
     event.preventDefault();
   });
 
@@ -27,30 +29,37 @@ $(document).ready(function() {
   });
 
   $("#q4").submit(function(event) {
-    $("#q4").hide();
-    $("#q5").show();
-
+    var input4 = parseInt($("#activities").val());
+    if (input4 === 1) {
+      $("#q4-alert").text("Please make a selection");
+    } else {
+      $("#q4").hide();
+      $("#q5").show();
+    }
     event.preventDefault();
   });
 
   $("#q5").submit(function(event) {
-    $("#q5").hide();
-
+    var q1Input = $("#name").val();
     var q2Input = parseInt($("#season").val());
+    var q3Input = $("#dob").val();
     var q4Input = parseInt($("#activities").val());
     var q5Input = parseInt($("#travel").val());
     var totalPoints = (q2Input + q4Input + q5Input);
 
-    if (totalPoints <= 6) {
-      $("#vacation1").show();
-    } else if (totalPoints >= 7 && totalPoints <=12) {
-      $("#vacation2").show();
-    } else if (totalPoints >=13) {
-      $("#vacation3").show();
+    if (q5Input === 1) {
+      $("#q5-alert").text("Please make a selection");
+    } else {
+      $("#q5").hide();
+      if (totalPoints <= 8) {
+        $("#vacation1").show();
+      } else if (totalPoints >= 9 && totalPoints <=12) {
+        $("#vacation2").show();
+      } else if (totalPoints >=13) {
+        $("#vacation3").show();
+      }
     }
-
-    var nameInput = $("#name").val();
-    $(".user-name").text(nameInput);
+    $(".user-name").text(q1Input);
     event.preventDefault();
   });
 });
