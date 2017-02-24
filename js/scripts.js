c3a45cdf0e00c5e5cff9bc551ee77e69ca137d07
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-02-24 13:27:50 -0800

    adjust vacation options, add more if statements so forms work properly on mobile devices and add a button to return to the first question

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-
-
 $(document).ready(function() {
   $("select").children("option:first-child").hide();
+  //to hide the "Pick one" options from the DOM
 
   $("#q1").submit(function(event) {
-    $("#q1").hide();
-    $("#q2").show();
-
+    var input1 = $("#name").val();
+    if (input1 === "") {
+      $("#q1-alert").text("Please enter your name");
+    } else {
+      $("#q1").hide();
+      $("#q2").show();
+    }
     event.preventDefault();
   });
 
@@ -22,14 +25,18 @@ $(document).ready(function() {
   });
 
   $("#q3").submit(function(event) {
-    $("#q3").hide();
-    $("#q4").show();
-
+    var input3 = $("#dob").val();
+    if (input3 === "") {
+      $("#q3-alert").text("Please enter your date of birth");
+    } else {
+      $("#q3").hide();
+      $("#q4").show();
+    }
     event.preventDefault();
   });
 
   $("#q4").submit(function(event) {
-    var input4 = parseInt($("#activities").val());
+    var input4 = parseInt($("#color").val());
     if (input4 === 1) {
       $("#q4-alert").text("Please make a selection");
     } else {
@@ -43,23 +50,32 @@ $(document).ready(function() {
     var q1Input = $("#name").val();
     var q2Input = parseInt($("#season").val());
     var q3Input = $("#dob").val();
-    var q4Input = parseInt($("#activities").val());
-    var q5Input = parseInt($("#travel").val());
+    var q4Input = parseInt($("#color").val());
+    var q5Input = parseInt($("#activity").val());
     var totalPoints = (q2Input + q4Input + q5Input);
 
     if (q5Input === 1) {
       $("#q5-alert").text("Please make a selection");
     } else {
       $("#q5").hide();
-      if (totalPoints <= 8) {
+      if (totalPoints <= 22) {
         $("#vacation1").show();
-      } else if (totalPoints >= 9 && totalPoints <=12) {
+      } else if (totalPoints >= 23 && totalPoints <=26) {
         $("#vacation2").show();
-      } else if (totalPoints >=13) {
+      } else if (totalPoints >= 27 && totalPoints <= 31) {
         $("#vacation3").show();
+      } else if (totalPoints >= 32) {
+        $("#vacation4").show();
       }
     }
+    $("#redo").show();
     $(".user-name").text(q1Input);
     event.preventDefault();
+  });
+
+  $("#redo").click(function() {
+    $(".vacation").hide();
+    $("#q1").show();
+    $("#redo").hide();
   });
 });
